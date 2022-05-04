05cd906d05e77c85c71317bb01b9a72a4cbe4c41
Aleksandra Szymon <lamonaleksandra@gmail.com>
2022-05-04 23:49:54 +0200

    UPDATE zawartosci
    pododawałam pliki tyle ile na razie zrobiłam

---
+++ EE.09-2021-01-02/futbol.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Rozgrywki futbolowe</title>
+	<link rel="stylesheet" type="text/css" href="styl.css">
+</head>
+<body>
+
+	<section id="baner">
+		
+		<h2>Światowe rozgrywski piłkarskie</h2>
+		<img src="obraz1.jpg" alt="boisko">
+
+	</section>
+
+	<section id="mecze"></section>
+
+	<section id="glowny">
+		
+		<h2>Reprezentacja Polski</h2>
+
+	</section>
+
+	<section id="lewy">
+		
+		<p>Podaj pozycję zawodników (1-bramkarze, 2-obrońcy, 3-pomocnicy, 4-napastnicy):</p>
+
+		<form method="POST" action="#">
+			
+			<input type="number" name="numer">
+			<input type="button" value="Sprawdź">
+
+		</form>
+
+		<ul></ul>
+
+	</section>
+
+	<section id="prawy">
+		
+		<img src="zad1.png" alt="piłkarz">
+		<p>Autor: 00000000000</p>
+
+	</section>
+
+</body>
+</html>
